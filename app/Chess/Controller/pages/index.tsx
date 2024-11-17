5fe6d3f78539c5167e81d12ce54575d3e9de3814
SirunGameDev <sirun.game.dev@gmail.com>
2024-11-17 22:32:35 +0100

    adding react with vite

---
+++ app/Chess/Controller/pages/index.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Index() {
+
+    return (
+        <main>
+          <h1>Solo Board</h1>
+          <p>Play with yourself on this board.</p>
+          
+        </main>
+      );
+}
